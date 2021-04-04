04d2c837a889db3dda65d56835136b202327b101
Steinny <charitymbisi@gmail.com>
2021-04-04 12:56:43 +0300

    open vector

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 
 enum class State {
     kEmpty,
-    kObstacle
+    kObstacle,
+    kClosed
 };
 std::string CellString(State state){
     switch(state){
@@ -53,26 +54,29 @@ std::vector<std::vector<State>> ReadBoardFile(std::string path){
     } else std::cout << "Could not open the file"<<std::endl;
     return board;
 }
-std::vector<std::vector<State>> Search(std::vector<std::vector<State>>board,int length[]){
-    std::vector<std::vector<State>> solution;
+std::vector<std::vector<State>> Search(int start[],int goal[]){
+    std::vector<std::vector<State>> board;
     std::cout <<"No path Found"<<std::endl;
-    return solution;
+    return board;
 }
 int heuristic(int x1,int y1,int x2,int y2){
     return abs(x2-x1) + abs(y2-y1);
 }
-void addToOpen(int x,int y,int g,int h,std::vector<std::vector<int>> open,std::vector<std::vector<State>> grid){
+void addToOpen(int x,int y,int g,int h,std::vector<std::vector<int>> &open,std::vector<std::vector<State>> &grid){
     std::vector<int> node ={x,y,g,h};
     open.push_back(node);
     //Set the grid value for the x and y coordinates to the enum value kClosed. We have added kClosed to the set of enum values.
+    grid[x].push_back(State::kClosed);
+    grid[y].push_back(State::kClosed);
+
 }
 int main() {
     auto board = ReadBoardFile("/home/charity/CLionProjects/Grids/resources/grids.txt");
     std::cout << "From grids file"<<std::endl;
     printBoard(board);
-    int length[2] = {4, 5};
-    //std::vector<std::vector<State>> start = {{0,0}};
-    std::vector<std::vector<State>> solution = Search({{}},length);
+    int start[2] ={0,0};
+    int goal[2] ={4,5};
+    std::vector<std::vector<State>> solution = Search(start,goal);
     printBoard(solution);
     int number = heuristic(3,5,2,3);
     std::cout <<number<<std::endl;
