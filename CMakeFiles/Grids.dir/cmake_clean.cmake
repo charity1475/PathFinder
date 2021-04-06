702c6fb1d939e1a70f1326c93bdbb06417ae12f3
Steinny <charitymbisi@gmail.com>
2021-04-06 12:27:53 +0300

    path planner

---
+++ CMakeFiles/Grids.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Grids.dir/main.cpp.o"
+  "Grids"
+  "Grids.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Grids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Maze.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Maze.dir/main.cpp.o"
+  "Maze"
+  "Maze.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Maze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
